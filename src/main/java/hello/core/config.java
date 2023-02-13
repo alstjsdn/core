9d981e9b8532f1f3d043ebaa880b495031f1620b
alstjsdn <alstjsdn99@naver.com>
2023-02-13 21:10:21 +0900

    Commit

---
+++ src/main/java/hello/core/config.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@EnableWebMvc
+public class config extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
